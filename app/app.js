d2764975673d2c70bee541f8210152cdf26aef73
Pablo <pablopasodiges@iMac.local>
2017-08-22 20:00:05 +0200

    Finnaly working upload!

--- app/app.js
+++ app/app.js
@@ -2,9 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -15,6 +12,8 @@ var fileUpload = require('express-fileupload');
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,16 +21,13 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload());
 
 app.use('/', index);
 app.use('/users', users);
 
+// Mine
 app.use('/upload', upload);
+
 
 app.listen(3000, function(){
     console.log('Server started...');
--- app/routes/upload.js
+++ app/routes/upload.js
@@ -1,27 +1,35 @@
 var express = require('express');
 var router = express.Router();
+path = require('path'),
+os = require('os'),
+fs = require('fs');
+
+
+var formidable = require('formidable');
+
 
 router.get('/', function(req, res, next) {
   res.render('upload');
 });
 
 router.post('/', function(req, res, next) {
-    console.log('Trying to upload file');
-   if (!req.files) {
-        console.log('not file indicated');
-      errors = {error : {msg:'No ha indicado un archivo'}};
-      res.render('upload');
-   }
-   let myFile = req.files.filename;
-   let response = "";
-   myFile.mv('../data/uploads', function(err) {
-           if (err)
-               return res.status(500).send(err);
-            });
-   console.log('File upload success!');
-   response = 'Archivo subido correctamente';
-   res.render('upload', {resp : response} );
-    
+  var form = new formidable.IncomingForm();
+  form.once('error', console.log);
+  form.uploadDir =
+  form.encoding = 'binary';
+  form.parse(req, function(err, fields, files) {
+    console.log('File upload success!');
+    if (err) throw err;
+    files.path = form.uploadDir + files.name;
+    response = 'Archivo subido correctamente';
+    res.render('upload', {resp : response} );
+  });
+  form.on('fileBegin', function(name, file){
+    file.path =  __dirname + '/../uploads/' + file.name;  
+  });
+  form.on('file', function(name, file){
+    console.log('Saved file ' + file.path)
+  });
 });
 
 module.exports = router;
